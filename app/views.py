2b21580ab542c9c9fee68deecba7dc04517710da
Carlos5Noob <cchaati2101@g.educaand.es>
2025-03-08 16:50:51 +0100

    primera vista creada. Lista de videojuegos

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Videojuego
 
 # Create your views here.
+
+class ListVideogames(ListView):
+    template_name = "app/lista_videojuegos.html"
+    context_object_name = 'videojuegos'
+
+    def get_queryset(self):
+        return  Videojuego.objects.all()
+
