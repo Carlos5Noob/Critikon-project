4e330a0cd8e9c2e8f3fa12a6e7f8870b5607f02a
Carlos5Noob <cchaati2101@g.educaand.es>
2025-03-08 16:49:26 +0100

    settings modificado para servir archivos media

--- critikon/settings.py
+++ critikon/settings.py
@@ -118,6 +118,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+
+# Configuración para archivos multimedia (imágenes subidas por los usuarios)
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
