94cc676ded1e8e5e806e9a3be4143d0a41330e5d
Carlos5Noob <cchaati2101@g.educaand.es>
2025-03-09 17:34:37 +0100

    registro tabla CustomUser en el admin

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from app.models import Videojuego, Opinion
+from app.models import Videojuego, Opinion, CustomUser
 
 # Register your models here.
 
 admin.site.register(Videojuego)
 admin.site.register(Opinion)
+admin.site.register(CustomUser)
