b6cddd375b4a1b7f5c0434a8cb59f8161dc97ad5
Carlos5Noob <cchaati2101@g.educaand.es>
2025-03-09 17:36:57 +0100

    nueva url para el formulario de reviews

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,6 @@ app_name = 'app'
 urlpatterns = [
     path('', views.ListVideogames.as_view(), name='videojuegos'),
     path('<int:game_id>/', views.lista_opiniones, name='lista_opiniones'),
+    path('opiniones-recientes/', views.ListOpinionsRecents.as_view(), name='opiniones_recientes'),
+    path('<int:game_id>/crear_review/', views.crear_review, name='crear_review'),
 ]
