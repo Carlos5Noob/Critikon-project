c4c73b50efe5a5ee97f297bf35fccb5a1d5f2ccb
Carlos5Noob <cchaati2101@g.educaand.es>
2025-04-19 19:26:10 +0200

    ViewSet de la clase Videojuego y ApiRoot

---
+++ api/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from app.models import Videojuego
+from .serializers import VideojuegoSerializer
+
+class VideojuegoViewSet(viewsets.ModelViewSet):
+    queryset = Videojuego.objects.all()
+    serializer_class = VideojuegoSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class ApiRootViewSet(viewsets.ViewSet):
+    def list(self, request):
+        return Response({
+            'videojuegos': request.build_absolute_uri(reverse('api:videojuego-list', request=request)),
+        })
