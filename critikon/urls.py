ad15d635a6954704861690209f80c8996a89d21e
Carlos5Noob <cchaati2101@g.educaand.es>
2025-04-19 20:05:22 +0200

    fix bug imágenes 404

--- critikon/urls.py
+++ critikon/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('accounts/', include('django.contrib.auth.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
