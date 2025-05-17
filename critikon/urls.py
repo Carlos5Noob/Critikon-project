ce0bbf6930976f0ca18fad949455e4a68e72397a
Carlos5Noob <cchaati2101@g.educaand.es>
2025-05-17 14:20:17 +0200

    add url home '/'

--- critikon/urls.py
+++ critikon/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='inicio.html'), name='inicio'),
     path('admin/', admin.site.urls),
     path('critikon/', include('app.urls')),
     path('api/', include('api.urls')),
