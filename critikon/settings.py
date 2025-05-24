c31aa9ee75904e2de8723f8b5a25fc26faec2cf6
Carlos5Noob <cchaati2101@g.educaand.es>
2025-05-24 18:46:19 +0200

    csrf configurations

--- critikon/settings.py
+++ critikon/settings.py
@@ -16,6 +16,9 @@ from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ["https://critikon.carlos5noob.tech"]
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
